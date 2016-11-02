c38e2c77a8c038359d1bd69369188d94df815a29
Akash Devaraju <akash@digifutura.com>
2016-11-02 19:05:20 +0530

    [Search] Completed search feature

--- js/app.js
+++ js/app.js
@@ -23,6 +23,23 @@ $( document ).ready(function() {
     });
   }
 
+  $(".search").click(function(){
+    var boro_no = $('#borough-select').val();
+    var house_no = $('#house-no').val();
+    var street = $('#street').val();
+    var params = { 
+      boro: boro_no,
+      houseno: house_no,
+      street: street
+    }
+
+    var url = "http://a810-bisweb.nyc.gov/bisweb/PropertyProfileOverviewServlet?" + $.param(params)
+
+    var redirectWindow = window.open(url, '_blank');
+    redirectWindow.location;
+  });
+
+
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
