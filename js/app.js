d88ef19298f67df882ed60c188756fa0b5df8279
Akash Devaraju <akash@digifutura.com>
2016-11-08 17:11:45 +0530

    [Fixes] Added counter for violation fields

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ $( document ).ready(function() {
       top: 760
     }
   });
-  count=3
+  var count=3
 
   $(".add-btn").click(function(){
-    $(".violation-input").append('<input type="text" placeholder="Violation Number"  name="violation" class="mar-20 mar-small" id=' + '"violation-' + count + '">' )
+    $(".violation-input").append('<input type="text" placeholder="Violation Number" class="mar-20 mar-small" name=' + '"violation-' + count + '">' )
+    count = count+1;
   });
 
   var path = window.location.pathname
